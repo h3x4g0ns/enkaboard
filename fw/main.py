40d7d0ecd3827a23bb9d2ca870041e08a16ce1ba
h3x4g0ns <agrawalekansh02@gmail.com>
2023-06-12 22:24:11 -0700

    coded keymap

--- fw/main.py
+++ fw/main.py
@@ -17,15 +17,15 @@ split = Split(
 keyboard.modules.append(split)
 
 keyboard.keymap = [
-    [   #0q
-        KC.ESC,  KC.N1,   KC.N2,   KC.N3, KC.N4, KC.N5,                             KC.N6, KC.N7,   KC.N8,  KC.N9,   KC.N0,  KC.GRV,
-        KC.TAB,   KC.Q,    KC.W,    KC.E,  KC.R,  KC.T,                              KC.Y,  KC.U,    KC.I,   KC.O,    KC.P, KC.BSLS,
-        KC.LSFT,  KC.A,    KC.S,    KC.D,  KC.F,  KC.G,                              KC.H,  KC.J,    KC.K,   KC.L, KC.SCLN, KC.RSFT,
-        KC.LCTL,  KC.Z,    KC.X,    KC.C,  KC.V,  KC.B,                              KC.N,  KC.M, KC.COMM, KC.DOT, KC.SLSH, KC.RCTL,
-                        KC.LEFT, KC.RGHT,                                                           KC.UP, KC.DOWN,
-                                              KC.MO(1),  KC.SPC,         KC.ENT, KC.MO(2),
-                                               KC.LALT,  KC.DEL,        KC.BSPC,  KC.LALT,
-                                               KC.HOME, KC.LGUI,        KC.RGUI,  KC.END
+    [   #0
+        KC.ESC,  KC.N1,    KC.N2,   KC.N3, KC.N4, KC.N5,                             KC.N6, KC.N7,   KC.N8,  KC.N9,   KC.N0, KC.BSPC,
+        KC.GRV,   KC.Q,    KC.W,    KC.E,  KC.R,  KC.T,                              KC.Y,  KC.U,    KC.I,   KC.O,    KC.P, KC.BSLS,
+        KC.LSFT,  KC.A,    KC.S,    KC.D,  KC.F,  KC.G,                              KC.H,  KC.J,    KC.K,   KC.L, KC.SCLN, KC.QUOT,
+        KC.CAPS,  KC.Z,    KC.X,    KC.C,  KC.V,  KC.B,                              KC.N,  KC.M, KC.COMM, KC.DOT, KC.SLSH, KC.RSFT,
+                        KC.LBRC, KC.RBRC,                                                           KC.MINS, KC.EQL,
+                                            KC.LGUI,  KC.LSFT,          KC.SPC,     KC.ENT,
+                                            KC.LCTL,  KC.LALT,          KC.UP,  KC.RIGHT,
+                                            KC.MO(1), KC.DEL,           KC.LEFT,  KC.DOWN
     ],
     [  #1
         KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5, KC.F6,                           KC.F7, KC.F8, KC.F9,  KC.F10,  KC.F11,  KC.F12,
@@ -36,16 +36,6 @@ keyboard.keymap = [
                                                  KC.TRNS, KC.TRNS,       KC.TRNS,   KC.NO,
                                                  KC.TRNS, KC.TRNS,       KC.TRNS, KC.TRNS,
                                                  KC.TRNS, KC.TRNS,       KC.TRNS, KC.TRNS
-    ],
-    [  #2
-        KC.F13,  KC.F14,   KC.F15,  KC.F16,  KC.F17, KC.F18,                         KC.F19,   KC.F20, KC.F21,  KC.F22,  KC.F23,  KC.F24,
-        KC.TRNS, KC.NO,     KC.NO,   KC.NO,   KC.NO,  KC.NO,                          KC.NO,    KC.NO,  KC.NO,   KC.NO,   KC.NO,   KC.NO,
-        KC.TRNS, KC.MINS, KC.PLUS, KC.LBRC, KC.RBRC,  KC.NO,                          KC.NO, KC.RESET,  KC.NO,   KC.NO,  KC.RLD, KC.TRNS,
-        KC.TRNS, KC.EQL,  KC.UNDS, KC.LCBR, KC.RCBR,  KC.NO,                          KC.NO,    KC.NO,  KC.NO,   KC.NO,   KC.NO, KC.TRNS,
-                            KC.NO,   KC.NO,                                                                   KC.BRIU, KC.BRID,
-                                                      KC.NO, KC.TRNS,      KC.TRNS, KC.TRNS,
-                                                    KC.TRNS, KC.TRNS,      KC.TRNS, KC.TRNS,
-                                                    KC.TRNS, KC.TRNS,      KC.TRNS, KC.TRNS
     ]
 ]
 
