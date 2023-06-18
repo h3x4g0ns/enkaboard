97a18de5eaae8d1bdb714ce68abaa67283fca7f4
h3x4g0ns <agrawalekansh02@gmail.com>
2023-06-18 11:05:32 -0700

    tab

--- fw/main.py
+++ fw/main.py
@@ -20,7 +20,7 @@ keyboard.keymap = [
     [   #0
         KC.ESC,  KC.N1,    KC.N2,   KC.N3, KC.N4, KC.N5,                             KC.N6, KC.N7,   KC.N8,  KC.N9,   KC.N0, KC.BSPC,
         KC.GRV,   KC.Q,    KC.W,    KC.E,  KC.R,  KC.T,                              KC.Y,  KC.U,    KC.I,   KC.O,    KC.P, KC.BSLS,
-        KC.LSFT,  KC.A,    KC.S,    KC.D,  KC.F,  KC.G,                              KC.H,  KC.J,    KC.K,   KC.L, KC.SCLN, KC.QUOT,
+        KC.TAB,  KC.A,    KC.S,    KC.D,  KC.F,  KC.G,                              KC.H,  KC.J,    KC.K,   KC.L, KC.SCLN, KC.QUOT,
         KC.CAPS,  KC.Z,    KC.X,    KC.C,  KC.V,  KC.B,                              KC.N,  KC.M, KC.COMM, KC.DOT, KC.SLSH, KC.RSFT,
                         KC.LBRC, KC.RBRC,                                                           KC.MINS, KC.EQL,
                                             KC.LGUI,  KC.LSFT,          KC.SPC,     KC.ENT,
